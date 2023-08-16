0326efa153977eb6da7f88389a879df69475d02e
goldenplume <valle.francesco@gmail.com>
2023-08-16 02:42:45 +0200

    [feat] Aggiunta bean per incapsulare stato sessione del grano rosario.

---
+++ app/src/main/java/com/example/myrosarioprayer/support/RosarioGranoBean.java
@@ -0,0 +1,14 @@
+package com.example.myrosarioprayer.support;
+
+public class RosarioGranoBean {
+
+    public RosarioGranoBean() {}
+
+    private String granoTesto;
+    private Integer granoProgressivo;
+
+    private String granoSymbol;
+
+    private String granoGraphicSymbol;
+
+}
